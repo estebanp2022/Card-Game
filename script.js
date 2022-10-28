dd2468666b13ccee530658df0c52a6bf1d53bd87
estebanp2022 <91852753+estebanp2022@users.noreply.github.com>
2022-10-27 21:46:39 -0400

    game logic

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let newDeckBtn = document.querySelector(".new-deck-btn");
 let drawCards = document.querySelector(".draw-cards");
-
+let header = document.querySelector(".winner");
+let remainingCards = document.querySelector(".remaining");
+let cardsContainer = document.querySelector("cards");
+let computerScoreEl = document.querySelector(".computer-score");
+let playerScoreEl = document.querySelector(".player-score");
 let deckId;
+let computerScore = 0;
+let playerScore = 0;
+
 function handleClick() {
   fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
+      remainingCards.textContent = data.remaining;
       console.log(data);
       deckId = data.deck_id;
     });
@@ -20,9 +28,57 @@ function draw2Cards() {
         <img src="${data.cards[0].image}"/>
         <img src="${data.cards[1].image}"/>
       `;
+
+      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+
+      header.textContent = winnerText;
+      remainingCards.textContent = data.remaining;
+
+      if (data.remaining === 0) {
+        drawCards.disabled = true;
+        if (computerScore > playerScore) {
+          header.textContent = "The Computer Won!";
+        } else if (playerScore > computerScore) {
+          header.textContent = "You Won!";
+        } else {
+          header.textContent = "Its a tie!";
+        }
+      }
     });
 }
 
 newDeckBtn.addEventListener("click", handleClick); //handleClick is a callback function we created earlier
 
 drawCards.addEventListener("click", draw2Cards);
+
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+
+  if (card1ValueIndex > card2ValueIndex) {
+    computerScore++;
+    computerScoreEl.textContent = `${computerScore}`;
+    return "Computer gets point!";
+  } else if (card1ValueIndex < card2ValueIndex) {
+    playerScore++;
+    playerScoreEl.textContent = `${playerScore}`;
+    return "You get a point!";
+  } else {
+    return "It's a tie!";
+  }
+}
